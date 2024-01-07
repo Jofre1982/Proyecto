a40ddbabbd1ce3934b352122db8cc0b6e3779a2c
ISXCVXS\augus <augustochacon2007@gmail.com>
2024-01-07 05:08:18 -0500

    termino el codigo del inicio con poo

--- inicio_programacion_POO+registro.py
+++ inicio_programacion_POO+registro.py
@@ -22,87 +22,87 @@ class Conexion:
 
 class Estudiante:
     def __init__(self):
-        self.documento = None
+        self.EstudianteID = None
         self.nombre = None
         self.apellido = None
-        self.fecha_nacimiento = None
+        self.FechaNacimiento = None
         self.direccion = None
         self.telefono = None
-        self.correo = None
-        self.grado = None
+        self.CorreoElectronico = None
+        self.CursoID = None
 
     def ingresar_datos(self):
-        self.documento = input("Digite su documento de identificación: ")
+        self.EstudianteID = input("Digite su EstudianteID de identificación: ")
         self.nombre = input("Digite su nombre: ")
         self.apellido = input("Digite su apellido: ")
-        self.fecha_nacimiento = input("Digite su fecha de nacimiento: ")
+        self.FechaNacimiento = input("Digite su fecha de nacimiento: ")
         self.direccion = input("Digite su dirección: ")
         self.telefono = input("Digite su teléfono: ")
-        self.correo = input("Digite su correo: ")
-        self.grado = input("Digite su grado: ")
+        self.CorreoElectronico = input("Digite su CorreoElectronico: ")
+        self.CursoID = input("Digite su CursoID: ")
 
     def guardar_datos(self, conexion):
         cursor = conexion.conexion.cursor()
-        consulta = "INSERT INTO Estudiante (Documento, Nombre, Apellido, FechaNacimiento, Direccion, Telefono, Correo, Grado) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-        datos = (self.documento, self.nombre, self.apellido, self.fecha_nacimiento, self.direccion, self.telefono, self.correo, self.grado)
+        consulta = "INSERT INTO Estudiante (EstudianteID, Nombre, Apellido, FechaNacimiento, Direccion, Telefono, CorreoElectronico, CursoID) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        datos = (self.EstudianteID, self.nombre, self.apellido, self.FechaNacimiento, self.direccion, self.telefono, self.CorreoElectronico, self.CursoID)
         cursor.execute(consulta, datos)
         conexion.conexion.commit()
         cursor.close()
 
 class Profesor:
     def __init__(self):
-        self.documento = None
+        self.ProfesorID = None
         self.nombre = None
         self.apellido = None
-        self.fecha_nacimiento = None
+        self.FechaNacimiento = None
         self.direccion = None
         self.telefono = None
-        self.correo = None
+        self.CorreoElectronico = None
         self.especialidad = None
 
     def ingresar_datos(self):
-        self.documento = input("Digite su documento de identificación: ")
+        self.ProfesorID = input("Digite su ProfesorID de identificación: ")
         self.nombre = input("Digite su nombre: ")
         self.apellido = input("Digite su apellido: ")
-        self.fecha_nacimiento = input("Digite su fecha de nacimiento: ")
+        self.FechaNacimiento = input("Digite su fecha de nacimiento: ")
         self.direccion = input("Digite su dirección: ")
         self.telefono = input("Digite su teléfono: ")
-        self.correo = input("Digite su correo: ")
+        self.CorreoElectronico = input("Digite su CorreoElectronico: ")
         self.especialidad = input("Digite su materia de especialidad: ")
 
     def guardar_datos(self, conexion):
         cursor = conexion.conexion.cursor()
-        consulta = "INSERT INTO Profesor (Documento, Nombre, Apellido, FechaNacimiento, Direccion, Telefono, Correo, Especialidad) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-        datos = (self.documento, self.nombre, self.apellido, self.fecha_nacimiento, self.direccion, self.telefono, self.correo, self.especialidad)
+        consulta = "INSERT INTO Profesor (ProfesorID, Nombre, Apellido, FechaNacimiento, Direccion, Telefono, CorreoElectronico, Especialidad) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        datos = (self.ProfesorID, self.nombre, self.apellido, self.FechaNacimiento, self.direccion, self.telefono, self.CorreoElectronico, self.especialidad)
         cursor.execute(consulta, datos)
         conexion.conexion.commit()
         cursor.close()
 
 class Administrativo:
     def __init__(self):
-        self.documento = None
+        self.AdministrativoID = None
         self.nombre = None
         self.apellido = None
-        self.fecha_nacimiento = None
+        self.FechaNacimiento = None
         self.direccion = None
         self.telefono = None
-        self.correo = None
+        self.CorreoElectronico = None
         self.puesto = None
 
     def ingresar_datos(self):
-        self.documento = input("Digite su documento de identificación: ")
+        self.AdministrativoID = input("Digite su AdministrativoID de identificación: ")
         self.nombre = input("Digite su nombre: ")
         self.apellido = input("Digite su apellido: ")
-        self.fecha_nacimiento = input("Digite su fecha de nacimiento: ")
+        self.FechaNacimiento = input("Digite su fecha de nacimiento: ")
         self.direccion = input("Digite su dirección: ")
         self.telefono = input("Digite su teléfono: ")
-        self.correo = input("Digite su correo: ")
+        self.CorreoElectronico = input("Digite su CorreoElectronico: ")
         self.puesto = input("Digite su puesto: ")
 
     def guardar_datos(self, conexion):
         cursor = conexion.conexion.cursor()
-        consulta = "INSERT INTO Administrativo (Documento, Nombre, Apellido, FechaNacimiento, Direccion, Telefono, Correo, Puesto) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-        datos = (self.documento, self.nombre, self.apellido, self.fecha_nacimiento, self.direccion, self.telefono, self.correo, self.puesto)
+        consulta = "INSERT INTO Administrativo (AdministrativoID, Nombre, Apellido, FechaNacimiento, Direccion, Telefono, CorreoElectronico, Puesto) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        datos = (self.AdministrativoID, self.nombre, self.apellido, self.FechaNacimiento, self.direccion, self.telefono, self.CorreoElectronico, self.puesto)
         cursor.execute(consulta, datos)
         conexion.conexion.commit()
         cursor.close()
@@ -110,46 +110,47 @@ class Administrativo:
 def verificar_entidad(usuario, contrasena, conexion):
     cursor = conexion.conexion.cursor()
 
-    # comparar datos en la tabla estudiante
-    consulta_estudiante = "SELECT EstudianteID FROM Estudiante WHERE Nombre = %s AND Contraseña = %s"
-    cursor.execute(consulta_estudiante, (usuario, contrasena))
-    resultado_estudiante = cursor.fetchone()
-    if resultado_estudiante:
-        return "Estudiante"
+    # comparar datos en la tabla Administrativo
+    consulta_Administrativo = "SELECT AdministrativoID FROM Administrativo WHERE ID = %s AND Nombre = %s"
+    cursor.execute(consulta_Administrativo, (contrasena, usuario))
+    resultado_Administrativo = cursor.fetchone()
+    if resultado_Administrativo:
+        return "Administrativo"
 
     # comparar datos en la tabla profesor
-    consulta_profesor = "SELECT ProfesorID FROM Profesor WHERE Nombre = %s AND Contraseña = %s"
-    cursor.execute(consulta_profesor, (usuario, contrasena))
+    consulta_profesor = "SELECT ProfesorID FROM Profesor WHERE ID = %s AND Nombre = %s"
+    cursor.execute(consulta_profesor, (contrasena, usuario))
     resultado_profesor = cursor.fetchone()
     if resultado_profesor:
         return "Profesor"
 
-    # comparar datos en la tabla administrativo
-    consulta_administrativo = "SELECT AdministrativoID FROM Administrativo WHERE Nombre = %s AND Contraseña = %s"
-    cursor.execute(consulta_administrativo, (usuario, contrasena))
-    resultado_administrativo = cursor.fetchone()
-    if resultado_administrativo:
-        return "Administrativo"
+    # comparar datos en la tabla estudiante
+    consulta_estudiante = "SELECT EstudianteID FROM Estudiante WHERE ID = %s AND Nombre = %s"
+    cursor.execute(consulta_estudiante, (contrasena, usuario))
+    resultado_estudiante = cursor.fetchone()
+    if resultado_estudiante:
+        return "Estudiante"
     
     cursor.close()
     return "No se encontró ninguna entidad con los datos de inicio de sesión proporcionados"
+
 
 def registrarse(rol):
     conexion = Conexion()
     conexion.establecer_conexion()
 
-    if rol == "estudiante":
-        estudiante = Estudiante()
-        estudiante.ingresar_datos()
-        estudiante.guardar_datos(conexion)
+    if rol == "Administrativo":
+        administrativo = Administrativo()
+        administrativo.ingresar_datos()
+        administrativo.guardar_datos(conexion)
     elif rol == "profesor":
         profesor = Profesor()
         profesor.ingresar_datos()
         profesor.guardar_datos(conexion)
-    elif rol == "administrativo":
-        administrativo = Administrativo()
-        administrativo.ingresar_datos()
-        administrativo.guardar_datos(conexion)
+    elif rol == "estudiante":
+        estudiante = Estudiante()
+        estudiante.ingresar_datos()
+        estudiante.guardar_datos(conexion)
 
     conexion.cerrar_conexion()
 
@@ -165,5 +166,5 @@ def home(opcion):
     if opcion == "iniciar sesion":
         inicio_sesion()
     elif opcion == "registrarse":
-        rol = input("Ingrese su rol (estudiante, profesor, administrativo): ")
+        rol = input("Ingrese su rol (Administrativo, profesor, estudiante): ")
         registrarse(rol)
